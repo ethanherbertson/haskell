eafd6b5ff62373ae8bb9c50aed8f8d4118f4f019
Ethan Herbertson <ejherbertson@msts.com>
2019-04-05 19:55:13 +0000

    More work on PigLatin

--- exercises/pig-latin/src/PigLatin.hs
+++ exercises/pig-latin/src/PigLatin.hs
@@ -1,6 +1,22 @@
 module PigLatin (translate) where
 
+adday :: String -> String
+adday xs = xs ++ "ay"
+
+moveone :: String -> String
+moveone (x:xs) = xs ++ [x]
+moveone (xs) = xs
+
+movetwo :: String -> String
+movetwo = moveone . moveone
+
+movethree :: String -> String
+movethree = moveone . movetwo
+
 translate :: String -> String
+translate (x:y:z:rest) | elem x "aeiou"           = adday ([x, y, z] ++ rest)
+                       | elem [x, y] ["ch", "qu"] = (adday . movetwo) ([x, y, z] ++ rest)
+                       | [y, z] == "qu"           = (adday . movethree) ([x, y, z] ++ rest)
+                       | True                     = (adday . moveone) ([x, y, z] ++ rest)
 translate [] = []
-translate (x:xs) | elem x "aeiou"                         = [x] ++ xs ++ "ay"
-                 | True                                   = xs ++ [x] ++ "ay"
+translate (xs) = xs ++ "ay"
