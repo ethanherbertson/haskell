ab5dfd2badf7bfbbd48a1f3d64dbf67aae5158be
Ethan Herbertson <ejherbertson@msts.com>
2019-04-12 21:14:19 +0000

    All pig-latin tests now passing

--- exercises/pig-latin/src/PigLatin.hs
+++ exercises/pig-latin/src/PigLatin.hs
@@ -14,12 +14,37 @@ moveTwo = moveOne . moveOne -- efficiency?
 moveThree :: String -> String
 moveThree = moveOne . moveTwo -- efficiency?
 
--- TODO: Should probably handle upper/lower case...
+moveFour :: String -> String
+moveFour = moveTwo . moveTwo -- efficiency?
+
+move :: Int -> String -> String
+move 1 xs = moveOne xs
+move 2 xs = moveTwo xs
+move 3 xs = moveThree xs
+move 4 xs = moveFour xs
+move _ xs = xs
+
+isVowel :: Char -> Bool
+isVowel x = elem x "aeiouAEIOU"
+
+isVowelOrY :: Char -> Bool
+isVowelOrY x = elem x "aeiouyAEIOUY"
+
+leadingConsonants :: String -> String
+leadingConsonants (x:xs)    | isVowel x = ""
+                            | otherwise = [x] ++ (takeWhile (not . isVowelOrY) xs)
+leadingConsonants "" = ""
+
+lastConsFirstVowel :: String -> String
+lastConsFirstVowel xs   | length (leadingConsonants xs) == 0    = [head xs]
+                        | otherwise                             = last (leadingConsonants xs) : head (filter isVowel xs) : []
+
+charsUntilVowel :: String -> Int
+charsUntilVowel = length . leadingConsonants
+
 translate :: String -> String
-translate s@(x:y:z:_) | elem ' ' s               = unwords (map translate (words s))
-                      | elem x "aeiou"           = theFonz s
-                      | elem [x, y] ["yt", "xr"] = theFonz s
-                      | elem [x, y] ["qu"]       = (theFonz . moveTwo) s
-                      | [y, z] == "qu"           = (theFonz . moveThree) s
-                      | True                     = (theFonz . moveOne) s
-translate xs = theFonz xs
+translate xs    | elem ' ' xs                                   = unwords (map translate (words xs))
+                | elem (leadingConsonants xs) ["xr", "yttr"]    = theFonz xs
+                | (lastConsFirstVowel xs == "qu")               = (theFonz . (move (1 + (charsUntilVowel xs)))) xs
+                | otherwise                                     = (theFonz . (move (charsUntilVowel xs))) xs
+
